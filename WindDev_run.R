2f9d7f6d00af2350b0e93862d35fb46dc80e3b03
margaux <msleckman@ucsb.edu>
2018-09-19 14:25:20 -0700

    e

--- WindDev_run.R
+++ WindDev_run.R
@@ -16,8 +16,7 @@ Windbelt_Df <- read_csv("Windbelt Project Database.csv", header =T)
 Windbelt_df_3years <- Windbelt_Df %>% 
   filter()
   
-pal <- c("red", "green", "blue", "yellow", "orange", 
-                      "purple", "grey")
+pal <- colorFactor(palette = unique(as.character(Windbelt_Df$`Owner Type`)))
 
 factpal <- colorFactor(pal, Windbelt_Df$`Owner Type`)
 qpal <- colorQuantile("Blues", Windbelt_Df$`Owner Type`, n = 7)
@@ -28,10 +27,10 @@ m <- leaflet(Windbelt_Df) %>%
              lng = Windbelt_Df$Longitude, 
              popup = as.character(Windbelt_Df$`Master Project Name`),
              weight = 3, radius = 30, 
-             color = ~factpal(`Owner Type`)) %>% 
-  addLegend("bottomright", colors= "red",
-            labels="wind power projects",
-            title="Wind Projects in the Wind Belt")
+             color = ~pal(`Owner Type`)) %>% 
+addLegend("bottomright", colors =  pal, values = ~`Owner Type`)
+            # labels="wind power projects",
+            # title="Wind Projects in the Wind Belt")
   m
   
   
